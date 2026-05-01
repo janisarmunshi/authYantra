77869abe546681eb4a5d841147b4884256f3666e
janisarmunshi <janisar.munshi@gmail.com>
2026-05-01 00:02:03 +0000

    blank change

--- auth-service/migrations/env.py
+++ auth-service/migrations/env.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from config import get_settings
 from models import Base
-
+ 
 # this is the Alembic Config object
 config = context.config
 
